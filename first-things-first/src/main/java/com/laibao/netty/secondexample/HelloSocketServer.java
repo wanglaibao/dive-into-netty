a6329c4409698297f3e9377fad7280d2f8a510fb
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-01-10 14:48:16 +0800

    update pom.xml file info

---
+++ first-things-first/src/main/java/com/laibao/netty/secondexample/HelloSocketServer.java
@@ -0,0 +1,33 @@
+package com.laibao.netty.secondexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class HelloSocketServer {
+
+    public static void main(String[] args){
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                            .channel(NioServerSocketChannel.class).
+                            handler(new LoggingHandler(LogLevel.WARN))
+                            .childHandler(new HelloSocketServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
