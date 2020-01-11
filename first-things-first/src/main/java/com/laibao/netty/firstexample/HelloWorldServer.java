768d40afd6d56bd24c8dadf32396510662dcb28e
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-01-11 08:25:28 +0800

    update pom.xml file info

--- first-things-first/src/main/java/com/laibao/netty/firstexample/HelloWorldServer.java
+++ first-things-first/src/main/java/com/laibao/netty/firstexample/HelloWorldServer.java
@@ -10,24 +10,52 @@ public class HelloWorldServer {
 
     public static void main(String[] args) {
 
+        /**
+         * 定义两个事件循环组，就是死循环
+         */
         EventLoopGroup bossGroup = null;
         EventLoopGroup workerGroup = null;
         try {
+
+            /**
+             * 仅仅接受连接,转给workerGroup,自己不做处理
+             */
             bossGroup = new NioEventLoopGroup();
+
+            /**
+             * 处理客户端的连接请求以及IO读写操作
+             */
             workerGroup = new NioEventLoopGroup();
 
+            /**
+             * 服务端引导程序 用于配置启动以及监听客户端的连接
+             */
             ServerBootstrap serverBootstrap = new ServerBootstrap();
 
+
+            /**
+             * childHandler子处理器,传入一个初始化器【HelloWorldServerInitializer】
+             */
             serverBootstrap.group(bossGroup, workerGroup)
                             .channel(NioServerSocketChannel.class)
                             .childHandler(new HelloWorldServerInitializer());
 
+            /**
+             * 绑定一个端口并且同步，生成一个ChannelFuture对象
+             */
             ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+
+            /**
+             * 对关闭的监听
+             */
             channelFuture.channel().closeFuture().sync();
 
         }catch (Exception ex) {
             ex.printStackTrace();
         } finally {
+            /**
+             *  循环组优雅关闭
+             */
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
         }
--- first-things-first/src/main/java/com/laibao/netty/firstexample/HelloWorldServerInitializer.java
+++ first-things-first/src/main/java/com/laibao/netty/firstexample/HelloWorldServerInitializer.java
@@ -7,12 +7,27 @@ import io.netty.handler.codec.http.HttpServerCodec;
 
 public class HelloWorldServerInitializer extends ChannelInitializer<SocketChannel> {
 
+    /**
+     * 这是一个回调方法,在channel被注册时调用
+     * @param socketChannel
+     * @throws Exception
+     */
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
+        /**
+         * 管道链,可以在它里面添加进很多ChannelHandler
+         */
         ChannelPipeline channelPipeline = socketChannel.pipeline();
 
+        /**
+         * 添加一个Netty处理器 HttpServerCodec 完成http编解码
+         *
+         */
         channelPipeline.addLast("httpServerCodec", new HttpServerCodec());
 
+        /**
+         * 增加一个自己定义的处理器 HttpWorldServerHandler 用来处理真正的业务逻辑
+         */
         channelPipeline.addLast("testHttpServerHandler", new HttpWorldServerHandler());
     }
 
