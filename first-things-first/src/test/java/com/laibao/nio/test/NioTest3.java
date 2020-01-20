efd1a87bf74c51474c53a92423dcc67b122d38e2
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-01-21 00:21:21 +0800

    update pom.xml file info

---
+++ first-things-first/src/test/java/com/laibao/nio/test/NioTest3.java
@@ -0,0 +1,37 @@
+package com.laibao.nio.test;
+
+import org.junit.Test;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NioTest3 {
+
+    private static final String FILE_PATH = "D:\\IdeaProjects\\git\\dive-into-netty\\first-things-first\\src\\test\\resources\\NioTest3.txt";
+
+    @Test
+    public void testWriteDataToFile() throws IOException {
+
+        try( FileOutputStream fileOutputStream = new FileOutputStream(FILE_PATH)){
+
+            FileChannel fileChannel = fileOutputStream.getChannel();
+
+            ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+
+            byte[] messages = "hello world welcome, nihao".getBytes();
+
+            for(int i = 0; i < messages.length; ++i) {
+                byteBuffer.put(messages[i]);
+            }
+
+            byteBuffer.flip();
+
+            fileChannel.write(byteBuffer);
+        }
+
+
+
+    }
+}
