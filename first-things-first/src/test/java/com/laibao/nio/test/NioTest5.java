2abbf417ccf31d45ae3c664fff66e7b328fd3ebc
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-01-21 19:46:20 +0800

    update pom.xml file info

---
+++ first-things-first/src/test/java/com/laibao/nio/test/NioTest5.java
@@ -0,0 +1,33 @@
+package com.laibao.nio.test;
+
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+/**
+ * ByteBuffer类型化的put与get方法
+ */
+public class NioTest5 {
+
+
+    @Test
+    public void testPutAndGetMethod() {
+        ByteBuffer buffer = ByteBuffer.allocate(64);
+
+        buffer.putInt(15);
+        buffer.putLong(500000000L);
+        buffer.putDouble(14.123456);
+        buffer.putChar('你');
+        buffer.putShort((short)2);
+        buffer.putChar('我');
+
+        buffer.flip();
+
+        System.out.println(buffer.getInt());
+        System.out.println(buffer.getLong());
+        System.out.println(buffer.getDouble());
+        System.out.println(buffer.getChar());
+        System.out.println(buffer.getShort());
+        System.out.println(buffer.getChar());
+    }
+}
