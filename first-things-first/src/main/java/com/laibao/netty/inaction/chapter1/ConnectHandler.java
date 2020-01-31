7ec2147e305527494abdfc5f4c62358437cd6cc2
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-01-31 15:40:20 +0800

    update pom.xml file info

---
+++ first-things-first/src/main/java/com/laibao/netty/inaction/chapter1/ConnectHandler.java
@@ -0,0 +1,18 @@
+package com.laibao.netty.inaction.chapter1;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ *
+ *
+ * 被回调触发的 ChannelHandler
+ */
+public class ConnectHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    //当一个新的连接已经被建立时，channelActive(ChannelHandlerContext)将会被调用
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Client " + ctx.channel().remoteAddress() + " connected");
+    }
+}
