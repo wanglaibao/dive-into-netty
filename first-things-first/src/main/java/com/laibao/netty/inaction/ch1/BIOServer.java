a241c360a7f3b56c3b6f707932c94ef1ff7bcdf0
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-02 08:31:56 +0800

    update pom.xml file info

---
+++ first-things-first/src/main/java/com/laibao/netty/inaction/ch1/BIOServer.java
@@ -0,0 +1,60 @@
+package com.laibao.netty.inaction.ch1;
+
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * BIOServer端首先创建了一个serverSocket来监听8000端口,
+ * 然后创建一个线程,线程里面不断调用阻塞方法serversocket.accept()来获取新的连接见(2),
+ * 当获取到新的连接之后,给每条连接创建一个新的线程,这个线程负责从该连接中读取数据,见(3),
+ * 然后读取数据是以字节流的方式见(4).
+ */
+public class BIOServer {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8000);
+
+            /**
+             * (1)接收新连接线程
+             */
+            new Thread(() -> {
+                while (true) {
+
+                    try {
+                        /**
+                         * (2)阻塞方法获取新的连接
+                         */
+                        Socket socket = serverSocket.accept();
+
+                        /**
+                         * (3)每一个新的连接都创建一个线程，负责读取数据
+                         */
+                        new Thread(() -> {
+                            try {
+                                int len;
+                                byte[] data = new byte[1024];
+                                InputStream inputStream = socket.getInputStream();
+
+                                /**
+                                 * (4)按字节流方式读取数据
+                                 */
+                                while ((len = inputStream.read(data)) != -1) {
+                                    System.out.println(new String(data, 0, len));
+                                }
+                            } catch (Exception ex) {
+                                ex.printStackTrace();
+                            }
+                        }).start();
+
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }).start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
