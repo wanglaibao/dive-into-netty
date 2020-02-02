50f1b0119f78d4b4d303e1c8adbeeaff38db9fe1
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-02 09:24:19 +0800

    update pom.xml file info

---
+++ first-things-first/src/main/java/com/laibao/netty/inaction/ch1/NettyClient.java
@@ -0,0 +1,38 @@
+package com.laibao.netty.inaction.ch1;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.time.LocalDate;
+
+public class NettyClient {
+
+    public static void main(String[] args) {
+        Bootstrap bootstrap = new Bootstrap();
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                });
+
+        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
+
+        while (true) {
+            channel.writeAndFlush(LocalDate.now() + ": hello world!");
+            try {
+                Thread.sleep(2000);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
---
+++ first-things-first/src/main/java/com/laibao/netty/inaction/ch1/NettyServer.java
@@ -0,0 +1,37 @@
+package com.laibao.netty.inaction.ch1;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class NettyServer {
+
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        NioEventLoopGroup bossGrouup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        serverBootstrap
+                .group(bossGrouup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel socketChannel) {
+                        ChannelPipeline pipeline = socketChannel.pipeline();
+                        pipeline.addLast(new StringDecoder());
+                        pipeline.addLast(new SimpleChannelInboundHandler<String>() {
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, String msg) {
+                                System.out.println(msg);
+                            }
+                        });
+                    }
+                })
+                .bind(8000);
+    }
+}
