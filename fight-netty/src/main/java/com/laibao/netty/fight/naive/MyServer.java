ce8c0430d5fc8dbd43ff992da275a1ba028fce75
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-04 18:47:25 +0800

    update pom.xml file info

---
+++ fight-netty/src/main/java/com/laibao/netty/fight/naive/MyServer.java
@@ -0,0 +1,30 @@
+package com.laibao.netty.fight.naive;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class MyServer {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new MyServerInitializer());
+
+        try {
+            ChannelFuture channelFuture = bootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+}
---
+++ fight-netty/src/main/java/com/laibao/netty/fight/naive/MyServerHandler.java
@@ -0,0 +1,21 @@
+package com.laibao.netty.fight.naive;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.UUID;
+
+public class MyServerHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String msg) throws Exception {
+        System.out.println(channelHandlerContext.channel().remoteAddress()+","+msg);
+        channelHandlerContext.channel().writeAndFlush("from NettyServer"+ UUID.randomUUID());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
