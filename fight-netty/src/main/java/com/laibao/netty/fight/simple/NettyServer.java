3167a179860f2147b58f0299901abade1d787fef
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-04 17:38:02 +0800

    update pom.xml file info

---
+++ fight-netty/src/main/java/com/laibao/netty/fight/simple/NettyServer.java
@@ -0,0 +1,98 @@
+package com.laibao.netty.fight.simple;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyServer {
+
+    public static void main(String[] args) throws Exception {
+
+        /**
+         * 创建BossGroup 和 WorkerGroup
+         * 1. 创建两个线程组 bossGroup 和 workerGroup
+         * 2. bossGroup 只是处理连接请求 , 真正的和客户端业务处理，会交给 workerGroup完成
+         * 3. 两个都是无限循环
+         * 4. bossGroup 和 workerGroup 含有的子线程(NioEventLoop)的个数 默认实际 cpu核数 * 2
+         *
+         */
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            /**
+             * 创建服务器端的引导程序对象，并且通过链式调用的方式进行参数配置
+             */
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            /**
+             * 设置两个线程组
+             */
+            bootstrap.group(bossGroup, workerGroup)
+                    /**
+                     * 使用NioServerSocketChannel作为服务器的通道实现
+                     */
+                    .channel(NioServerSocketChannel.class)
+                    /**
+                     * // 设置线程队列得到连接个数
+                     */
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    /**
+                     * //设置保持活动连接状态
+                     */
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    /**
+                     * // 该 handler对应 bossGroup , childHandler 对应 workerGroup
+                     */
+                    .handler(null)
+                    /**
+                     * 给我们的workerGroup的NioEventLoop对应的管道设置处理器
+                     */
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {//创建一个通道初始化对象(匿名对象)
+                        //给pipeline 设置处理器
+                        @Override
+                        protected void initChannel(NioSocketChannel socketChannel) throws Exception {
+                            ChannelPipeline pipeline = socketChannel.pipeline();
+                            System.out.println("客户socketchannel hashcode=" + socketChannel.hashCode());
+
+                            //可以使用一个集合管理SocketChannel,再推送消息时,可以将业务加入到各个channel对应的NIOEventLoop的taskQueue或者scheduleTaskQueue
+                            pipeline.addLast(new NettyServerHandler());
+                        }
+                    });
+
+            System.out.println(".....服务器 is ready...");
+
+            /**
+             * 绑定一个端口并且同步,生成了一个ChannelFuture对象
+             * 启动服务器(并绑定端口)
+             */
+            ChannelFuture channelFuture = bootstrap.bind(6668).sync();
+
+            /**
+             * 给channelFuture注册监听器，监控我们关心的事件
+             */
+            channelFuture.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (channelFuture.isSuccess()) {
+                        System.out.println("监听端口 6668 成功");
+                    } else {
+                        System.out.println("监听端口 6668 失败");
+                    }
+                }
+            });
+
+            /**
+             * 对关闭通道进行监听
+             */
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            /**
+             * 优雅关闭
+             */
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+}
