9aee151f944c6d649f541695dc0b5e44e43ebf86
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-04 19:26:54 +0800

    update pom.xml file info

---
+++ fight-netty/src/main/java/com/laibao/netty/fight/naive/MyClientHandler.java
@@ -0,0 +1,27 @@
+package com.laibao.netty.fight.naive;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.time.LocalDate;
+
+public class MyClientHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String msg) throws Exception {
+        System.out.println(channelHandlerContext.channel().remoteAddress());
+        System.out.println("client output"+ msg);
+        channelHandlerContext.writeAndFlush("from Client"+ LocalDate.now());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush("MD");
+    }
+}
