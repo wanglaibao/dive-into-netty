8eb26d0f6034fd12c7e9aa598b51b6c66c17e76d
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-07 02:09:00 +0800

    update pom.xml file info

--- fight-netty/src/main/java/com/laibao/netty/fight/bio/BIOServer.java
+++ fight-netty/src/main/java/com/laibao/netty/fight/bio/BIOServer.java
@@ -3,8 +3,7 @@ package com.laibao.netty.fight.bio;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class BIOServer {
 
@@ -16,7 +15,11 @@ public class BIOServer {
         //1. 创建一个线程池
         //2. 如果有客户端连接，就创建一个线程，与之通讯(单独写一个方法)
 
-        ExecutorService newCachedThreadPool = Executors.newCachedThreadPool();
+        //ExecutorService newCachedThreadPool = Executors.newCachedThreadPool();
+
+        int theadsNumber = Runtime.getRuntime().availableProcessors();
+
+        ExecutorService threadPool = new ThreadPoolExecutor(theadsNumber,theadsNumber,0, TimeUnit.SECONDS,new ArrayBlockingQueue(10));
 
         //创建ServerSocket
         ServerSocket serverSocket = new ServerSocket(6666);
@@ -32,12 +35,16 @@ public class BIOServer {
             System.out.println("连接到一个客户端");
 
             //就创建一个线程，与之通讯(单独写一个方法)
+            /*
             newCachedThreadPool.execute(new Runnable() {
                 public void run() { //我们重写
                     //可以和客户端通讯
                     handler(socket);
                 }
             });
+            */
+
+            threadPool.execute(() -> handler(socket));
 
         }
 
@@ -53,6 +60,14 @@ public class BIOServer {
             //通过socket 获取输入流
             InputStream inputStream = socket.getInputStream();
 
+            /*  其实这样写应该更简洁一点
+            int read0;
+            while ((read0 =  inputStream.read(bytes)) != -1) {
+                System.out.println("线程信息 id =" + Thread.currentThread().getId() + " 名字=" + Thread.currentThread().getName());
+                System.out.println("read....");
+                System.out.println(new String(bytes, 0, read0)); //输出客户端发送的数据
+            }*/
+
             //循环的读取客户端发送的数据
             while (true) {
 
@@ -61,8 +76,7 @@ public class BIOServer {
                 System.out.println("read....");
                int read =  inputStream.read(bytes);
                if(read != -1) {
-                   System.out.println(new String(bytes, 0, read
-                   )); //输出客户端发送的数据
+                   System.out.println(new String(bytes, 0, read)); //输出客户端发送的数据
                } else {
                    break;
                }
