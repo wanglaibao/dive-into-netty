90c5137daab09e9356c258485c0c2bb44b49c5d2
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-10 00:18:36 +0800

    update pom.xml file info

--- fight-netty/src/test/java/com/laibao/netty/fight/test/NanoTimeTest.java
+++ fight-netty/src/test/java/com/laibao/netty/fight/test/NanoTimeTest.java
@@ -14,4 +14,21 @@ public class NanoTimeTest {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testNanoTimeTwo(String[] args){
+        System.out.println(System.nanoTime());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(System.nanoTime());
+
+        int a = 10;
+        int b = 20;
+        int c = 10;
+        c -= a - b; // c = c - (a-b) = c - a + b = 10 - 10 + 20
+        System.out.println(c);
+    }
 }
