f9488e97b9645b7ff016a7c9c101150b3bec63ad
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-11 10:31:50 +0800

    update pom.xml file info

---
+++ java8_recipes/src/main/java/com/laibao/java8/recipes/optionals/Company.java
@@ -0,0 +1,15 @@
+package com.laibao.java8.recipes.optionals;
+
+import java.util.Optional;
+
+public class Company {
+    private Department department;
+
+    public Optional<Department> getDepartment() {
+        return Optional.ofNullable(department);
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
