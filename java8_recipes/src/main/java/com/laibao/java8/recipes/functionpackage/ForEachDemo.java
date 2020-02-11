11453a443b5c22e0079424b5e3fa74d0ac98bedb
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-11 10:35:22 +0800

    update pom.xml file info

---
+++ java8_recipes/src/main/java/com/laibao/java8/recipes/functionpackage/ForEachDemo.java
@@ -0,0 +1,26 @@
+package com.laibao.java8.recipes.functionpackage;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ForEachDemo {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("This", "is", "a", "list", "of", "strings");
+
+        strings.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        });
+
+        //noinspection Convert2MethodRef
+        strings.forEach(s -> System.out.println(s));
+
+        strings.forEach(System.out::println);
+
+        Consumer<String> printer = System.out::println;
+        strings.forEach(printer);
+    }
+}
