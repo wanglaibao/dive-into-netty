180a89c44f48b4176f819e1393633af912b75e5e
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-11 11:00:56 +0800

    update pom.xml file info

---
+++ java8_recipes/src/test/java/com/laibao/java8/recipes/defaults/CompanyEmployeeTest.java
@@ -0,0 +1,16 @@
+package com.laibao.java8.recipes.defaults;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyEmployeeTest {
+
+    @Test
+    void getName() {
+        CompanyEmployee peterGibbons = new CompanyEmployee("Peter", "Gibbons");
+
+        assertEquals("Peter Gibbons works for Initech",
+                peterGibbons.getName());
+    }
+}
