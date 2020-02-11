e5eea734569fc616a2341e344a2008b05684aa5f
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-11 10:42:26 +0800

    update pom.xml file info

---
+++ java8_recipes/src/main/java/com/laibao/java8/recipes/collectors/Movie.java
@@ -0,0 +1,20 @@
+package com.laibao.java8.recipes.collectors;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Movie {
+    private Set<Actor> actors = new HashSet<>();
+
+    public void addActor(Actor actor) {
+        actors.add(actor);
+    }
+
+    public void setActors(Set<Actor> actors) {
+        this.actors = actors;
+    }
+
+    public Set<Actor> getActors() {
+        return actors;
+    }
+}
