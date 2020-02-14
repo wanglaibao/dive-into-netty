c7ad83668a03d0e9ab18285577c42a4262137939
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-14 22:48:56 +0800

    update pom.xml file info

--- java-io-and-socket/src/test/java/com/laibao/nio/chapter1/test/ChannelTest.java
+++ java-io-and-socket/src/test/java/com/laibao/nio/chapter1/test/ChannelTest.java
@@ -20,10 +20,11 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-/*
+/**
  * 一、通道（Channel）：用于源节点与目标节点的连接。在 Java NIO 中负责缓冲区中数据的传输。Channel 本身不存储数据，因此需要配合缓冲区进行传输。
  * 
  * 二、通道的主要实现类
+ *
  * 	java.nio.channels.Channel 接口：
  * 		|--FileChannel
  * 		|--SocketChannel
@@ -31,6 +32,7 @@ import java.util.Set;
  * 		|--DatagramChannel
  * 
  * 三、获取通道
+ *
  * 1. Java 针对支持通道的类提供了 getChannel() 方法
  * 		本地 IO：
  * 		FileInputStream/FileOutputStream
@@ -42,21 +44,29 @@ import java.util.Set;
  * 		DatagramSocket
  * 		
  * 2. 在 JDK 1.7 中的 NIO.2 针对各个通道提供了静态方法 open()
+ *
  * 3. 在 JDK 1.7 中的 NIO.2 的 Files 工具类的 newByteChannel()
  * 
- * 四、通道之间的数据传输
- * transferFrom()
- * transferTo()
+ * 四、通道之间的数据传输【利用到了直接内存缓冲】
+ *
+ * 		transferFrom()
+ *
+ * 		transferTo()
  * 
  * 五、分散(Scatter)与聚集(Gather)
- * 分散读取（Scattering Reads）：将通道中的数据分散到多个缓冲区中
- * 聚集写入（Gathering Writes）：将多个缓冲区中的数据聚集到通道中
+ *
+ * 		分散读取（Scattering Reads）：将通道中的数据分散到多个缓冲区中
+ *
+ * 		聚集写入（Gathering Writes）：将多个缓冲区中的数据聚集到通道中
  * 
  * 六、字符集：Charset
- * 编码：字符串 -> 字节数组
- * 解码：字节数组  -> 字符串
+ *
+ * 		编码：字符串 -> 字节数组
+ *
+ * 		解码：字节数组  -> 字符串
  * 
  */
+
 public class ChannelTest {
 	
 	//字符集
@@ -105,7 +115,10 @@ public class ChannelTest {
 		}
 	}
 	
-	//分散和聚集
+	/**
+	 * 分散(Scatter)与聚集(Gather)
+	 * @throws IOException
+	 */
 	@Test
 	public void test4() throws IOException{
 		RandomAccessFile raf1 = new RandomAccessFile("1.txt", "rw");
@@ -136,20 +149,28 @@ public class ChannelTest {
 		channel2.write(bufs);
 	}
 	
-	//通道之间的数据传输(直接缓冲区)
+	/**
+	 * 通道之间的数据传输(直接缓冲区)
+	 * 		transferTo()
+	 * 		transferFrom()
+	 * @throws IOException
+	 */
 	@Test
 	public void test3() throws IOException{
 		FileChannel inChannel = FileChannel.open(Paths.get("d:/1.mkv"), StandardOpenOption.READ);
 		FileChannel outChannel = FileChannel.open(Paths.get("d:/2.mkv"), StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.CREATE);
 		
-//		inChannel.transferTo(0, inChannel.size(), outChannel);
+		//inChannel.transferTo(0, inChannel.size(), outChannel);
 		outChannel.transferFrom(inChannel, 0, inChannel.size());
 		
 		inChannel.close();
 		outChannel.close();
 	}
 	
-	//使用直接缓冲区完成文件的复制(内存映射文件)
+	/**
+	 * 使用直接缓冲区完成文件的复制(内存映射文件)
+	 * @throws IOException
+	 */
 	@Test
 	public void test2() throws IOException{//2127-1902-1777
 		long start = System.currentTimeMillis();
@@ -173,7 +194,9 @@ public class ChannelTest {
 		System.out.println("耗费时间为：" + (end - start));
 	}
 	
-	//利用通道完成文件的复制（非直接缓冲区）
+	/**
+	 * 利用通道完成文件的复制（非直接缓冲区）
+	 */
 	@Test
 	public void test1(){//10874-10953
 		long start = System.currentTimeMillis();
