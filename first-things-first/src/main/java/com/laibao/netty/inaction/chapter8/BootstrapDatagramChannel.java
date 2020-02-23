b1e099c7e253d3e714edb4a9ac115a822832334b
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-23 15:33:02 +0800

    update pom.xml file info

---
+++ first-things-first/src/main/java/com/laibao/netty/inaction/chapter8/BootstrapDatagramChannel.java
@@ -0,0 +1,61 @@
+package com.laibao.netty.inaction.chapter8;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.channel.socket.oio.OioDatagramChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * 使用 Bootstrap 和 DatagramChannel
+ *
+ */
+public class BootstrapDatagramChannel {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *  使用 Bootstrap 和 DatagramChannel
+     */
+    public void bootstrap() {
+        //创建一个 Bootstrap 的实例以创建和绑定新的数据报 Channel
+        Bootstrap bootstrap = new Bootstrap();
+        //设置 EventLoopGroup，其提供了用以处理 Channel 事件的 EventLoop
+        bootstrap.group(new OioEventLoopGroup()).channel(
+                //指定 Channel 的实现
+                OioDatagramChannel.class).handler(
+                //设置用以处理 Channel 的 I/O 以及数据的 ChannelInboundHandler
+                new SimpleChannelInboundHandler<DatagramPacket>() {
+                    @Override
+                    public void channelRead0(ChannelHandlerContext ctx,
+                                             DatagramPacket msg) throws Exception {
+                        // Do something with the packet
+                    }
+                }
+        );
+
+        //调用 bind() 方法，因为该协议是无连接的
+        ChannelFuture future = bootstrap.bind(new InetSocketAddress(0));
+
+        future.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture)
+                    throws Exception {
+                if (channelFuture.isSuccess()) {
+                    System.out.println("Channel bound");
+                } else {
+                    System.err.println("Bind attempt failed");
+                    channelFuture.cause().printStackTrace();
+                }
+            }
+        });
+    }
+}
