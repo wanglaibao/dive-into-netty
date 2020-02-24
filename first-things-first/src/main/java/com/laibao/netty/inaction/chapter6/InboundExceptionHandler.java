757e51735489792f08855163b59b77c3e71153f2
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-24 11:53:28 +0800

    update pom.xml file info

---
+++ first-things-first/src/main/java/com/laibao/netty/inaction/chapter6/InboundExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.laibao.netty.inaction.chapter6;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ *  基本的入站异常处理
+ *
+ */
+public class InboundExceptionHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx,
+                                Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
