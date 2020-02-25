f51c7e694413bf71b7dfee370ce5873fa2773225
wanglaibao2010 <wanglaibao2010@gmail.com>
2020-02-25 10:48:53 +0800

    update pom.xml file info

---
+++ first-things-first/src/main/java/com/laibao/netty/inaction/chapter10/CombinedByteCharCodec.java
@@ -0,0 +1,11 @@
+package com.laibao.netty.inaction.chapter10;
+
+import io.netty.channel.CombinedChannelDuplexHandler;
+
+public class CombinedByteCharCodec extends CombinedChannelDuplexHandler<ByteToCharDecoder, CharToByteEncoder> {
+
+    public CombinedByteCharCodec() {
+        //将委托实例传递给父类
+        super(new ByteToCharDecoder(), new CharToByteEncoder());
+    }
+}
